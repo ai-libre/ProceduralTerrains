0bcfdefa31c37297f1ad0c1d99887fcb2ac48582
gaetan <zyfodexe@gmail.com>
2026-06-23 16:23:50 +0200

    version update

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,5 +1,5 @@
 // Single source of truth for the app version. Display it everywhere from here.
-export const APP_VERSION = '0.7.1';
+export const APP_VERSION = '0.7.5';
 export const APP_NAME = 'Procedural Terrains';
 export const GITHUB_REPO_URL = 'https://github.com/ZyFou/ProceduralTerrains';
 export const AUTHOR_NAME = 'ZyFod';
